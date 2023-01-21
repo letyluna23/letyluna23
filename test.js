986fba7a2ae523a3e1dc57496b230e28138f2b7c
letyluna23 <94796491+letyluna23@users.noreply.github.com>
2023-01-21 13:21:02 +0200

    created addition function

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 const testFunction = () => {
     console.log('test')
 }
+
+const testOtherFunction = () => {
+    console.log('test other function')
+}
+
+const addition = (a, b) => {
+    return a + b
+}
